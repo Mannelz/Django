275f1c13d3285335cb8b4cbca418721b8c6d85fa
Mannelz <emannuelm29@gmail.com>
2024-06-04 12:50:41 -0300

    create customer card

--- website/views.py
+++ website/views.py
@@ -52,3 +52,11 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
 
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.error(request, 'Você precisa estar logado para acessar esta página!')
+        return redirect('login')
